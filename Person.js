cb480fa30661945b1dead423b2faf34524fee2ad
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-12 22:07:03 -0300

    Create person.js

---
+++ Person.js
@@ -0,0 +1,190 @@
+const peopleArray = [{
+        name: 'john',
+        age: 21,
+        job: 'developer',
+        address: {
+            city: 'Campinas',
+            street: 'Rua Wharever',
+            number: 351,
+            country: 'Brazil',
+        },
+        contactInfo: {
+            phone: '+5502119993255897',
+            email: 'johnwazoski@mail.com',
+        },
+        skills: [{
+                name: 'javascript',
+                experience: 5
+            },
+            {
+                name: 'java',
+                experience: 1
+            },
+            {
+                name: 'c#',
+                experience: 3
+            },
+            {
+                name: 'kubernetes',
+                experience: 1
+            },
+        ]
+    },
+    {
+        name: 'john',
+        age: 21,
+        job: 'developer',
+        address: {
+            city: 'Campinas',
+            street: 'Rua Wharever',
+            number: 351,
+            country: 'Brazil',
+        },
+        contactInfo: {
+            phone: '+5502119993255897',
+            email: 'johnwazoski@mail.com',
+        },
+        skills: [{
+                name: 'javascript',
+                experience: 5
+            },
+            {
+                name: 'java',
+                experience: 1
+            },
+            {
+                name: 'c#',
+                experience: 3
+            },
+            {
+                name: 'kubernetes',
+                experience: 1
+            },
+        ]
+    }, {
+        name: 'john',
+        age: 21,
+        job: 'developer',
+        address: {
+            city: 'Campinas',
+            street: 'Rua Wharever',
+            number: 351,
+            country: 'Brazil',
+        },
+        contactInfo: {
+            phone: '+5502119993255897',
+            email: 'johnwazoski@mail.com',
+        },
+        skills: [{
+                name: 'javascript',
+                experience: 5
+            },
+            {
+                name: 'java',
+                experience: 1
+            },
+            {
+                name: 'c#',
+                experience: 3
+            },
+            {
+                name: 'kubernetes',
+                experience: 1
+            },
+        ]
+    }, {
+        name: 'john',
+        age: 21,
+        job: 'developer',
+        address: {
+            city: 'Campinas',
+            street: 'Rua Wharever',
+            number: 351,
+            country: 'Brazil',
+        },
+        contactInfo: {
+            phone: '+5502119993255897',
+            email: 'johnwazoski@mail.com',
+        },
+        skills: [{
+                name: 'javascript',
+                experience: 5
+            },
+            {
+                name: 'java',
+                experience: 1
+            },
+            {
+                name: 'c#',
+                experience: 3
+            },
+            {
+                name: 'kubernetes',
+                experience: 1
+            },
+        ]
+    }, {
+        name: 'john',
+        age: 21,
+        job: 'developer',
+        address: {
+            city: 'Campinas',
+            street: 'Rua Wharever',
+            number: 351,
+            country: 'Brazil',
+        },
+        contactInfo: {
+            phone: '+5502119993255897',
+            email: 'johnwazoski@mail.com',
+        },
+        skills: [{
+                name: 'javascript',
+                experience: 5
+            },
+            {
+                name: 'java',
+                experience: 1
+            },
+            {
+                name: 'c#',
+                experience: 3
+            },
+            {
+                name: 'kubernetes',
+                experience: 1
+            },
+        ]
+    }, {
+        name: 'john',
+        age: 21,
+        job: 'developer',
+        address: {
+            city: 'Campinas',
+            street: 'Rua Wharever',
+            number: 351,
+            country: 'Brazil',
+        },
+        contactInfo: {
+            phone: '+5502119993255897',
+            email: 'johnwazoski@mail.com',
+        },
+        skills: [{
+                name: 'javascript',
+                experience: 5
+            },
+            {
+                name: 'java',
+                experience: 1
+            },
+            {
+                name: 'c#',
+                experience: 3
+            },
+            {
+                name: 'kubernetes',
+                experience: 1
+            },
+        ]
+    },
+
+]
