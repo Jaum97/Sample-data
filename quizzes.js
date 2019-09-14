c5296c6509b4712afeb62afefa11459faadbb200
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-09-14 19:03:55 -0300

    create

---
+++ quizzes.js
@@ -0,0 +1,142 @@
+const quizzes = [
+  {
+    name: 'letters',
+    activities: [
+      {
+        number: 1,
+        name: 'abc',
+        alternatives: [
+          {
+            value: 'a'
+          },
+          {
+            value: 'b'
+          },
+          {
+            value: 'c'
+          }
+        ]
+      },
+      {
+        number: 2,
+        name: '123',
+        alternatives: [
+          {
+            value: '1'
+          },
+          {
+            value: '2'
+          },
+          {
+            value: '3'
+          }
+        ]
+      },
+    ] 
+  },
+  {
+    name: 'letters',
+    activities: [
+      {
+        number: 1,
+        name: 'abc',
+        alternatives: [
+          {
+            value: 'a'
+          },
+          {
+            value: 'b'
+          },
+          {
+            value: 'c'
+          }
+        ]
+      },
+      {
+        number: 2,
+        name: 'def',
+        alternatives: [
+          {
+            value: 'd'
+          },
+          {
+            value: 'e'
+          },
+          {
+            value: 'f'
+          }
+        ]
+      },
+    ] 
+  },
+  {
+    name: 'letters',
+    activities: [
+      {
+        number: 1,
+        name: 'abc',
+        alternatives: [
+          {
+            value: 'a'
+          },
+          {
+            value: 'b'
+          },
+          {
+            value: 'c'
+          }
+        ]
+      },
+      {
+        number: 2,
+        name: '123',
+        alternatives: [
+          {
+            value: '1'
+          },
+          {
+            value: '2'
+          },
+          {
+            value: '3'
+          }
+        ]
+      },
+    ] 
+  },
+  {
+    name: 'numbers',
+    activities: [
+      {
+        number: 1,
+        name: '123',
+        alternatives: [
+          {
+            value: '1'
+          },
+          {
+            value: '2'
+          },
+          {
+            value: '3'
+          }
+        ]
+      },
+      {
+        number: 2,
+        name: '456',
+        alternatives: [
+          {
+            value: '4'
+          },
+          {
+            value: '5'
+          },
+          {
+            value: '6'
+          }
+        ]
+      },
+    ] 
+  },
+]
